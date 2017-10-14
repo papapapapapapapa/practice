24dca5280f0e38a213a23acc12c1f69092c37ab3
papapapapapapapa <r3386588@gmail.com>
2017-10-14 16:35:48 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in=new Scanner(System.in);
+        String string;
+        string=in.next();
+        System.out.println((char)Integer.parseInt(string,16));
+
+
+    }
+}
